c1c0179c17f25ec44a401599c5ad610c292965df
Mohammad Asef Mohammadi <mo.asef.mohammadi@gmail.com>
2026-05-22 11:33:22 +0200

    updat

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ randomNumber1 = Math.floor(Math.random() * 6) + 1;
 document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
 randomNumber2 = Math.floor(Math.random() * 6) + 1;
 document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 wins!";
+}else if(randomNumber1 == randomNumber2){
+    document.querySelector("h1").innerHTML="Drow!";
+}else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 wins!";
+}
